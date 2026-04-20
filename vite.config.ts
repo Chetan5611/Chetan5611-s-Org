da846aa5517cb6b6c57ef97adb9cdd570266e271
Chetan5611 <mahajanchetan5611@gmail.com>
2026-04-20 16:13:39 +0530

    Add base path configuration to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { defineConfig, loadEnv } from 'vite';
 
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
-  
+  base: './', // Add this line!
   return {
     plugins: [react(), tailwindcss()],
     define: {
